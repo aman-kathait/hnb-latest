d92a026ce35cf6e49c5567a3f31140f808159edc
Abhijeetrauthan111111 <abhijeetrauthan0@gmail.com>
2025-06-01 21:12:08 +0530

    @nd try deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import Signup from './components/signup'
-import MainLayout from './components/MainLayout'
-import Home from './components/Home'
-import Login from './components/Login'
+import Signup from './components/signup.jsx'
+import MainLayout from './components/MainLayout.jsx'
+import Home from './components/Home.jsx'
+import Login from './components/Login.jsx'
 import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
-import Profile from './components/Profile'
-import EditProfile from './components/EditProfile'
+import Profile from './components/Profile.jsx'
+import EditProfile from './components/EditProfile.jsx'
 import { useDispatch, useSelector } from 'react-redux'
-import Posts from './components/Posts'
-import Announcements from './components/Announcements'
-import Error from './components/Error'
-import ChatPage from './components/ChatPage'
+import Posts from './components/Posts.jsx'
+import Announcements from './components/Announcements.jsx'
+import Error from './components/Error.jsx'
+import ChatPage from './components/ChatPage.jsx'
 import {io} from "socket.io-client";
 import { use } from 'react'
 import { setSocket } from './redux/socketSlice'
 import { useEffect } from 'react'
 import { setLikeNotification } from './redux/rtnSlice'
-import ForgotPassword from "./components/auth/ForgotPassword";
-import VerifyResetCode from "./components/auth/VerifyResetCode";
-import ResetPassword from "./components/auth/ResetPassword";
+import ForgotPassword from "./components/auth/ForgotPassword.jsx";
+import VerifyResetCode from "./components/auth/VerifyResetCode.jsx";
+import ResetPassword from "./components/auth/ResetPassword.jsx";
 import useGetRTM from '@/hooks/useGetRTM';
-import TermsAndConditions from './components/TermsAndCondition'
-import PrivacyPolicy from './components/PrivacyPolicy'
+import TermsAndConditions from './components/TermsAndCondition.jsx'
+import PrivacyPolicy from './components/PrivacyPolicy.jsx'
 function ProtectedRoute({ children }) {
   const { user } = useSelector((store) => store.auth);
   if (!user) {
